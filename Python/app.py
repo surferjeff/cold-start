f68e8b61abf750c68dc67442a743e8b90a2a24b8
narusawa-taiga <137836504+narusawa-taiga@users.noreply.github.com>
2024-03-05 02:53:50 +0000

    finished web server for python

--- Python/app.py
+++ Python/app.py
@@ -6,24 +6,38 @@ import firebase_admin
 
 app = Flask(__name__)
 
+counter = 0
+
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 @app.route('/hello')
 def hello():
-    return 'Hello'
+    global counter
+    counter += 1
+
+    return jsonify({
+        "message": "Hello",
+        "requestCount": counter
+    })
 
 @app.route('/query_firestore', methods=['GET', 'POST'])
-def query_firestore():    
+def query_firestore():
+    global counter
     testing = db.collection('testing_data')
     docs = testing.stream()
-
+    
+    counter += 1
+    
     response = []
     for doc in docs:
         response.append(doc.to_dict())
         
-    return jsonify(response)
+    return jsonify({
+        'docs': response,
+        'requestCount': counter
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
