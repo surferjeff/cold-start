2717b4dcd56c62d13c8d8e1246749c8aeaec345f
narusawa-taiga <137836504+narusawa-taiga@users.noreply.github.com>
2024-03-17 05:02:22 +0000

    add .yaml and switched bucket

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,10 +1,3 @@
-terraform {
-    backend "gcs" {
-        bucket  = "coldstart-terraform-state"
-        prefix  = "terraform/state"
-    }
-}
-
 provider "google" {
     project = var.project_id
     region  = var.region
--- Terraform/version.tf
+++ Terraform/version.tf
@@ -1,7 +1,9 @@
 #### Configure backend ####
 terraform {
     required_version = ">= 0.14"
-    backend "gcs" {}
+    backend "gcs" {
+        bucket  = "coldstart-state-tf-state"
+    }
     required_providers {
         google = {
             source  = "hashicorp/google"
