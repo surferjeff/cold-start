694cac786f3fc90f5e862abd50e30d6a229781f2
narusawa-taiga <137836504+narusawa-taiga@users.noreply.github.com>
2024-04-04 02:35:30 +0000

    default template

--- Go/app.go
+++ Go/app.go
@@ -4,19 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
-	http.HandleFunc("/hello", helloWorld)
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	log.Printf("Server started on port %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	http.HandleFunc("/hello", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
