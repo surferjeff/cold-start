9ff16a1793651b2e05e816e298e49cb39874fb5b
narusawa-taiga <137836504+narusawa-taiga@users.noreply.github.com>
2024-04-05 01:37:08 +0000

    /hello handler

--- Go/app.go
+++ Go/app.go
@@ -1,16 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"encoding/json"
 	"log"
 	"net/http"
+	"sync"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+var (
+	counter int
+	firestoreClient *firestore.Client
+	mu sync.Mutex
+)
+
+func init() {
+	ctx := context.Background()
+	conf := &firebase.Config{ProjectID: "cold-start-417102"}
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		log.Fatalf("Failed to create firebase app: %v", err)
+	}
+
+	firestoreClient, err = app.Firestore(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create firestore client: %v", err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	counter++
+	mu.Unlock()
+
+	response := map[string]interface{}{
+		"message": "Hello, World!",
+		"requestCount": counter,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
-	http.HandleFunc("/hello", handler)
+	http.HandleFunc("/hello", helloHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
