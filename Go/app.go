710d03b50ea01f919c63fc21455e8bed6f6054a3
narusawa-taiga <137836504+narusawa-taiga@users.noreply.github.com>
2024-04-05 01:46:35 +0000

    WIP: query_firestore handler

--- Go/app.go
+++ Go/app.go
@@ -9,6 +9,7 @@ import (
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
+	"google.golang.org/api/iterator"
 )
 
 var (
@@ -45,7 +46,41 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func queryFirestoreHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	counter++
+	mu.Unlock()
+
+	var response []map[string]interface{}
+
+	ctx := context.Background()
+	iter := firestoreClient.Collection("testing_data").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Failed to iterate firestore documents: %v", err)
+			break
+		}
+
+		data := doc.Data()
+		response = append(response, data)
+	}
+
+	finalResponse := map[string]interface{}{
+		"docs": response,
+		"requestCount": counter,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(finalResponse)
+}
+
 func main() {
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/query_firestore", queryFirestoreHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
