8c33e7a325767f1dc510efcec65bc75cbd42b8c8
narusawa-taiga <137836504+narusawa-taiga@users.noreply.github.com>
2024-04-15 01:11:29 +0000

    tag coldstart-nodejs-115

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -133,5 +133,5 @@ module "nodejs" {
     code_repo_name = "github_narusawa-taiga_cold-start"
     build_api      = google_project_service.build_api
     run_api        = google_project_service.run_api
-    tag           = "coldstart-nodejs-113"
+    tag           = "coldstart-nodejs-114"
 }
