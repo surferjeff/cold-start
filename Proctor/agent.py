88158d449048002292a72c207c6b031e334ef579
narusawa-taiga <137836504+narusawa-taiga@users.noreply.github.com>
2024-04-18 23:40:33 +0000

    route and cloudbuild

--- Proctor/agent.py
+++ Proctor/agent.py
@@ -14,6 +14,15 @@ def hello():
     proctor.fetchAndLog("https://coldstart-java-ktbkw3cp2a-wl.a.run.app/hello")
     proctor.fetchAndLog("https://coldstart-nodejs-ktbkw3cp2a-wl.a.run.app/hello")
     return "Done"
+
+@app.route('/query_firestore')
+def query_firestore():
+    proctor.fetchAndLog("https://coldstart-python-ktbkw3cp2a-wl.a.run.app/query_firestore")
+    proctor.fetchAndLog("https://coldstart-csharp-ktbkw3cp2a-wl.a.run.app/query_firestore")
+    proctor.fetchAndLog("https://coldstart-go-ktbkw3cp2a-wl.a.run.app/query_firestore")
+    proctor.fetchAndLog("https://coldstart-java-ktbkw3cp2a-wl.a.run.app/query_firestore")
+    proctor.fetchAndLog("https://coldstart-nodejs-ktbkw3cp2a-wl.a.run.app/query_firestore")
+    return "Done"
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
