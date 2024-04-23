ccb938ae15ea476ae4f81d692cd0162084c9afbf
narusawa-taiga <137836504+narusawa-taiga@users.noreply.github.com>
2024-04-23 02:58:55 +0000

    coldstart-agent-111

--- Proctor/agent.py
+++ Proctor/agent.py
@@ -26,8 +26,10 @@ def query_firestore():
     return "Done"
     
 if __name__ == '__main__':
+    import logging
     client = google.cloud.logging.Client()
     client.setup_logging()
+    logging.basicConfig(level=logging.INFO)
     
     from waitress import serve
     serve(app, host='0.0.0.0', port=8080)
